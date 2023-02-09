1820237e97e5bb16d72b0643d0a480b1c1c020cb
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-09 17:30:01 +0545

    1 file modified

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const url = 'https://json-server--3000.local.webcontainer.io/comments';
 fetch(url)
   .then(r => r.json())
   .then((json) => {
-    console.log(JSON.stringify(json))
+    document.write(JSON.stringify(json))
   });
